80c284a259db1f8dac66cea172f0a3d568859eee
Subina00 <140357586+Subina00@users.noreply.github.com>
2023-09-12 09:40:57 +1000

    Marks Added

--- Assignment1.java
+++ Assignment1.java
@@ -16,9 +16,28 @@ public class Assignment1{
         //Allow the user to input the assignment name
         System.out.print("Enter the assignment name: ");
         String assignmentName = scanner.nextLine();
-
         
-    }
+        // F2: Allow the user to input students' marks for the assignment (up to 30 students)
+        ArrayList<Integer> marks = new ArrayList<>();
+        for (int i = 1; i <= 30; i++) {
+            while (true) {
+                try {
+                    System.out.print("Enter mark for student " + i + ": ");
+                    int mark = Integer.parseInt(scanner.nextLine());
+
+                    // Validate input as a valid mark (between 0 and 30)
+                    if (mark >= 0 && mark <= 30) {
+                        marks.add(mark);
+                        break;
+                    } else {
+                        System.out.println("Invalid mark. Please enter a mark between 0 and 30.");
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid input. Please enter a valid number.");
+                }
+            }
+        }
+}
 }
 
 
