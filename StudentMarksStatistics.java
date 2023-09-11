b2ef1b8fb78864725b414a787c5526d1e30bceb6
Subina00 <140357586+Subina00@users.noreply.github.com>
2023-09-12 09:38:02 +1000

    Added name

---
+++ StudentMarksStatistics.java
@@ -0,0 +1,25 @@
+
+/**
+ * This is the assignment1 to calculate the highest marks and lowest marks of the students and find the mean and standard deviation.
+ *
+ * @author (Subina)
+ * @version (Version 1.0 05/09/2023)
+ */
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StudentMarksStatistics {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //Allow the user to input the assignment name
+        System.out.print("Enter the assignment name: ");
+        String assignmentName = scanner.nextLine();
+
+        
+    }
+}
+
+
+
--- Subina_Assignment1/Assignment_1.java
+++
@@ -1,8 +0,0 @@
-
-/**
- * This is the assignment1 to calculate the highest marks and lowest marks of the students and find the mean and standard deviation.
- *
- * @author (Subina)
- * @version (Version 1.0 05/09/2023)
- */
-
