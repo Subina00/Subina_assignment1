99b4e0aca9d2faafed9ceac56dc30f11739b8976
Subina00 <140357586+Subina00@users.noreply.github.com>
2023-09-12 09:34:15 +1000

    .

--- Subina_Assignment1/Assignment_1.java
+++ Subina_Assignment1/Assignment_1.java
@@ -5,29 +5,4 @@
  * @author (Subina)
  * @version (Version 1.0 05/09/2023)
  */
-public class Assignment_1
-{
-    // instance variables - replace the example below with your own
-    private int x;
 
-    /**
-     * Constructor for objects of class Assignment_1
-     */
-    public Assignment_1()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
-}
